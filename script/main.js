749e283af44710c6d2700aa13f58e6b248d1fa72
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-23 15:25:58 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,31 @@
+/* 
+* OGGETTI
+Creare un oggetto che descriva uno studente.
+Lo studente avrà  le seguenti proprietà: nome, cognome e età.
+Stampare attraverso il for..in tutte le proprietà (chiavi e valori).
+*/
+
+$(document).ready(function() {
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
